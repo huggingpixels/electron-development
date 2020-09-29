752da736708d385e602f42fab90d4f499845ee58
Eszter Matyas <Eszter.Matyas@quest.com>
2020-09-29 15:40:06 +0200

    make app publishable

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const electron = require('electron');
 const path = require('path');
 const url = require('url');
-
+const { autoUpdater } = require("electron-updater");
 const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
@@ -22,7 +22,7 @@ app.on('ready', function () {
         protocol: 'file:',
         slashes: true
     }));
-    
+
     mainWindow.on('closed', function () {
         app.quit();
     })
@@ -41,3 +41,38 @@ const mainMenuTemplate = [
         label: 'File'
     }
 ]
+
+autoUpdater.on('checking-for-update', () => {
+    sendStatusToWindow('Checking for update...');
+});
+
+autoUpdater.on('update-available', (info) => {
+    sendStatusToWindow('Update available.');
+});
+
+autoUpdater.on('update-not-available', (info) => {
+    sendStatusToWindow('Update not available.');
+});
+
+autoUpdater.on('error', (err) => {
+    sendStatusToWindow('Error in auto-updater. ' + err);
+});
+
+autoUpdater.on('download-progress', (progressObj) => {
+    let log_message = "Download speed: " + progressObj.bytesPerSecond;
+    log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+    sendStatusToWindow(log_message);
+});
+
+autoUpdater.on('update-downloaded', (info) => {
+    sendStatusToWindow('Update downloaded');
+});
+
+app.on('ready', function () {
+    autoUpdater.checkForUpdatesAndNotify();
+});
+
+ipcMain.on('restart_app', () => {
+    autoUpdater.quitAndInstall();
+});
